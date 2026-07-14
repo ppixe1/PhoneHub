890862662edd5adf69b7bfc317375ca5d5926a52
ppixe1 <flovez1469@gmail.com>
2026-07-14 08:27:36 +0700

    Create Project structure

---
+++ Frontend/PhoneHub/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <h2>Hello World</h2>
+        <h3>Hello World</h3>
+        <p className='b1'>Hello World</p>
+        <p className='b2'>Hello World</p>
+        <p className='b3'>Hello World</p>
+      </div>
+    </>
+  )
+}
+
+export default App
